5996dbddaf7adf5f256442a8fd7ca64b688cf90a
chrisG759 <150283599+chrisG759@users.noreply.github.com>
2024-11-15 14:59:09 -0500

    first commit

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Welcome</title>
+    <style>
+        body{
+            text-align: center;
+        }
+        .linkButtons button{
+            width: 5cm;
+            height: 1cm;
+            border-radius: 1cm;
+            border: 0;
+            
+        }
+    </style>
+</head>
+<body>
+    <div class="greeting">
+        <h1></h1>
+    </div>
+    <div class="linkButtons">
+        <button>Login</button>
+        <button>Sign Up</button>
+    </div>
+</body>
+</html>
