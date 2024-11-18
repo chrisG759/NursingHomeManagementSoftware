1caf3e846b288dd130ff9e6a611b9bc4d5bc3d97
chrisG759 <150283599+chrisG759@users.noreply.github.com>
2024-11-18 14:09:35 -0500

    complete homepage with redirection to login/signup

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -9,6 +9,9 @@
     <style>
         body{
             text-align: center;
+            background-image:linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ), url('/homepageBackground.webp');
+            background-repeat: no-repeat;
+            background-size: cover;
         }
         .linkButtons button{
             width: 5cm;
@@ -20,20 +23,30 @@
         #login_but{
             background-color: rgb(70, 136, 172);
             color: white;
+            margin-right: 12px;
         }
         #login_but, #signup_but:hover{
             cursor: pointer;
         }
         .content{
+            display: flex;
+            flex-direction: column;
+            align-items: center;
             justify-content: center;
+            height: 100vh;
+        }
+        .greeting{
+            color: white;
+            margin-bottom: .5cm;
+            font-size: 24px;
         }
     </style>
 </head>
 <body>
     <div class="content">
         <div class="greeting">
-            <h2>Welcome to</h2>
-            <h2>Dream Team's Nursing Home</h2>
+            <h1>Welcome to</h1>
+            <h1>Dream Team's Nursing Home</h1>
         </div>
         <div class="linkButtons">
             <button id="login_but" onclick="window.location.href = '<?php echo route('login'); ?>'">Login</button>
